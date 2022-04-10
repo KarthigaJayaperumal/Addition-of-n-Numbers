d100610a52e5b8250e8b23f0a49e5174e6346cdd
Karthiga Jayaperumal <99970828+KarthigaJayaperumal@users.noreply.github.com>
2022-04-10 18:20:45 +0530

    Addition of n Numbers

---
+++ Practice.java
@@ -0,0 +1,17 @@
+package april_program; //Addition of n numbers
+
+public class Practice {
+
+	public static void main(String[] args) {
+		int no=1;
+		int total =0;
+		
+	while(no<=5) {
+		
+		total=total+no;
+		no++;
+	}
+	System.out.println("Addition of n numbers is " +total);
+	
+
+}}
